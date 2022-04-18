a0bc0d1351503524b676fbff9b1fa3995fccb4ab
Monica Calle <monicacalle369@gmail.com>
2022-04-18 01:59:01 +0000

    buliding a starwars rest apy with python

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
-#from models import Person
+from models import Character, Planet, Vehicle, Favorite
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -30,16 +30,78 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route("/characters", methods=["GET"])
+def get_characters():
+    try:
+        all_character = Character.query.all()
+        all_characters = list(map(lambda character: character.serialize(), all_character))
+        return jsonify(all_characters)
+    except Exception as error:
+        print(f"Character error : {error}")
+        return jsonify({"msg": "error occurred"})    
 
-    return jsonify(response_body), 200
+@app.route("/planets", methods=["GET"])
+def get_planets():
+    try:
+        all_planets = Planet.query.all()
+        all_planets = list(map(lambda planets: planets.serialize(), all_planets))
+        return jsonify(all_planets)
+    except Exception as error:
+        print(f"Planet error : {error}")
+        return jsonify({"msg": "error occurred"})
+
+@app.route("/vehicles", methods=["GET"])
+def get_vehicles():
+    try:
+        all_vehicles = Vehicle.query.all()
+        all_vehicles = list(map(lambda vehicles: vehicles.serialize(), all_starships))
+        return jsonify(all_vehicles)
+    except Exception as error:
+        print(f"Vehicles error : {error}")
+        return jsonify({"msg": "error occurred"})
+
+@app.route("/favorite", methods=["GET"])
+def get_favorite():
+    try:
+        all_favorite = Favorite.query.all()
+        all_favorite = list(map(lambda favorite: favorite.serialize(), all_favorite))
+        return jsonify(all_favorite)
+    except Exception as error:
+        print(f"Favorite error : {error}")
+        return jsonify({"msg": "error occurred"})
+
+@app.route("/favorite", methods=["POST"])
+def create_favorite():
+    try: 
+        favorite = Favorite()
+        body = request.get_json()
+        favorite.favorito = body["favorito"]       
+        db.session.add(favorite)
+        db.session.commit()
+        return jsonify({"favorite": body["favorito"]})
+    except Exception as error:
+        print(f"FavoritePOST error : {error}")
+        return jsonify({"msg": "error occurred"})
+
+
+@app.route("/favorite/<int:id>", methods=['DELETE'])
+def delete_favorites(id):
+    favorite = Favorite.query.filter_by(id=id)
+    if favorite is None:
+        return jsonify({"message": "Not Found"}), 404
+    deleted = favorite.delete()
+    if deleted == False:
+        return jsonify({"message": "Something happen try again"}), 500
+    return jsonify([]), 204
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
+    PORT = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+
+
+
+
+
+
--- src/models.py
+++ src/models.py
@@ -1,19 +1,99 @@
 from flask_sqlalchemy import SQLAlchemy
-
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(250),nullable=False)
+    nickname = db.Column(db.String(250),nullable=False)
+    email = db.Column(db.String(80),nullable=False)
+    password = db.Column(db.String(250),nullable=False)
 
-    def __repr__(self):
+    def __repr__ (self):
         return '<User %r>' % self.username
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
+            "nickname": self.nickname,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            # "password": self.password,
+        }
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250),nullable=False)
+    hair_color = db.Column(db.String(250), nullable=False)
+    eye_color = db.Column(db.String(250), nullable=False)
+    gender = db.Column(db.String(30), nullable=False)
+    dob = db.Column(db.String(30), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+
+    def __repr__ (self):
+        return '<Character %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "hair_color": self.hair_color,
+            "eye_color": self.eye_color,
+            "gender": self.gender,
+            "dob": self.dob,
+            "user_id": self.user_id,
+        }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    weather = db.Column(db.Integer, nullable=False)
+    gravity =  db.Column(db.Integer, nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+
+    def __repr__ (self):
+        return '<Planet %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "weather": self.weather,
+            "gravity": self.gravity,
+            "population": self.population,
+            "user_id": self.user_id,
+        }
+
+class Vehicle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    vehicle_class = db.Column(db.String(250), nullable=False)
+    manufacturer = db.Column(db.String(250), nullable=False)
+    cargo_capacity = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.ForeignKey('user.id'))
+
+    def __repr__ (self):
+        return '<Vehicle %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "vehicle_class": self.vehicle_class,
+            "manufacturer": self.manufacturer,
+            "cargo_capacity": self.cargo_capacity,
+            "user_id": self.user_id,
+            
+        }
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    favorite = db.Column(db.String(250))
+
+    def __repr__ (self):
+        return '<Favorite %r>' % self.favorite
+
+    def serialize(self):
+        return {
+            "favorite" : self.Favorite
         }
